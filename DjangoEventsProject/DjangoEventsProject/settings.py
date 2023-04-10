5ff3ee3c9eed397f12f2facc044cb8879b3317e3
Sanya Arora <sanya.arora@netsolutions.com>
2023-04-10 18:50:18 +0530

    envfile setup

--- DjangoEventsProject/DjangoEventsProject/settings.py
+++ DjangoEventsProject/DjangoEventsProject/settings.py
@@ -11,6 +11,12 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import environ
+import os
+
+
+env = environ.Env()
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,8 +26,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8c!g_97b(^kcn2cym(+n3wf+@16#k8#2yzj+)fz=)x0&g6b4&q'
 
+SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -81,11 +87,11 @@ WSGI_APPLICATION = 'DjangoEventsProject.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE':'django.db.backends.postgresql',
-        'NAME':'newdb',
-        'USER':'postgres',
-        'PASSWORD':'Radaur@1234',
-        'HOST':'127.0.0.1',
-        'PORT':'5432',
+        'NAME':env('DATABASE_NAME'),
+        'USER':env('DATABASE_USER'),
+        'PASSWORD':env('DATABASE_PASS'),
+        'HOST':env('DATABASE_HOST'),
+        'PORT':env('DATABASE_PORT'),
     }
 }
 
@@ -107,7 +113,7 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-AUTH_USER_MODEL='Accounts.Customuser'
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
--- DjangoEventsProject/Events/views.py
+++ DjangoEventsProject/Events/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render,redirect
+from rest_framework.authtoken.models import Token
+from django.contrib.auth.models import User
+from rest_framework.response import Response
+
+
+
 
 # Create your views here.
