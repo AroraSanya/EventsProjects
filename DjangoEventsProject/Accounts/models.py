fe6f2c149be7820c824f59cc61298ea6054de89e
Sanya Arora <sanya.arora@netsolutions.com>
2023-04-10 18:52:38 +0530

    envfile setupp

--- DjangoEventsProject/Accounts/models.py
+++ DjangoEventsProject/Accounts/models.py
@@ -1,15 +1,22 @@
 from django.db import models
 from django.utils import timezone
-from django.contrib.auth.models import AbstractUser
-
+from django.db import models
+from django.contrib.auth.models import User
 # Create your models here.
-class Customuser(AbstractUser):
+
+
+GENDER_CHOICES = [
+     ('M', 'Male'),
+     ('F', 'Female'),
+]
+class Profile(models.Model):
+     user = models.OneToOneField(User, on_delete=models.CASCADE)
      address = models.CharField(max_length=30, blank=True)
      email = models.CharField(max_length=30, blank=True, unique=True)
-     date_joined = models.DateTimeField(default=timezone.now)
-     created_at = models.DateTimeField(auto_now_add=True)
-     updated_at = models.DateTimeField(auto_now=True)
      age=models.IntegerField()
+     gender = models.CharField(max_length=1, choices=GENDER_CHOICES, default='M' )
      phone_no = models.CharField(max_length = 10)
-     USERNAME_FIELD ='email' # make the user log in with the email
-     REQUIRED_FIELDS = ['username','f_name','l_name']     
+
+         
+     USERNAME_FIELD = "email"
+     REQUIRED_FIELDS = ['first_name','last_name']
